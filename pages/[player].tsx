6fb46273c8c1c3ec77e043fd051c62a9b11b9b22
Jack LaFond <jack.lafond@icloud.com>
2021-06-27 00:04:32 -0400

    chore: fix K/D decimal

--- pages/[player].tsx
+++ pages/[player].tsx
@@ -53,17 +53,15 @@ export default function Post({ playerData, status }: Props) {
           </h1>
         </div>
         <div>
-          <ToolLink title="Stars" description={bedwars.stars} />
-
           <br></br>
           <ToolLink title="Total Kills" description={bedwars.kills_bedwars} />
           <ToolLink title="Total Deaths" description={bedwars.deaths_bedwars} />
           <ToolLink
             title="K/D Ratio"
             description={
-              Number(
-                bedwars.kills_bedwars / bedwars.deaths_bedwars
-              ) as unknown as string
+              (Math.floor(
+                Number(bedwars.kills_bedwars / bedwars.deaths_bedwars) * 100
+              ) / 100) as unknown as string
             }
           />
         </div>
