aae5bdb7cdcefe178f6a2a905c4154beff57268e
Jack LaFond <jack.lafond@icloud.com>
2021-06-26 23:34:30 -0400

    tool tip ting + api keys

--- pages/[player].tsx
+++ pages/[player].tsx
@@ -50,6 +50,9 @@ export default function Post({ playerData, status }: Props) {
             {status.online ? "online" : "offline"})
           </h1>
         </div>
+        <div>
+          <ToolLink title=" " description="bruh" />
+        </div>
       </main>
     </MainLayout>
   );
@@ -62,4 +65,14 @@ export async function getServerSideProps(params: {
   const playerData = await hypixel.player(player);
   const status = await hypixel.status(player);
   return { props: { playerData, status } };
+}
+function ToolLink(props: { title: string; description: string }) {
+  return (
+    <div className="float-left mb-2 mr-2">
+      <a className="transform hover:scale-95 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:hover:text-gray-300 shadow-full inline-block border hover:border-red-100 transition ease-in-out bg-gray-100 hover:bg-red-50 hover:text-red-500 rounded-md p-3 dark:text-gray-500 dark:bg-gray-800 dark:border-gray-700">
+        <h1 className="font-semibold text-sm sm:text-regular">{props.title}</h1>
+        <p className="text-xs">{props.description}</p>
+      </a>
+    </div>
+  );
 }
